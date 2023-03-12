8f658f9833e642aaf57fede7bccef81d3c4b8fa5
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2023-03-12 14:43:22 +0300

    added authorization function for http requests. RequestTypes php file was created.

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Authorizations;
+use App\ResponseTypes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AuthorizationsController extends Controller
 {
-    //
+    public function authorizing(Request $request)
+    {
+        $ip = $request->ip();
+        $auth = Authorizations::where("ip_address", $ip)->firstOrFail();
+
+        if($auth->ip_address != null)
+        {
+            return $auth->auth_token;
+        }
+        else
+        {
+            $auth_token = Str::uuid();
+            $auth = new Authorizations();
+            $auth->ip_address = $ip;
+            $auth->auth_token = $auth_token;
+            $auth->is_active = true;
+            $auth->expired_at = null;
+            $auth->save();
+            return [
+                "status" => ResponseTypes::$ok,
+                "auth_token" => $auth_token,
+            ];
+        }
+    }
 }
---
+++ app/ResponseTypes.php
@@ -0,0 +1,8 @@
+<?php
+    namespace App;
+
+    class ResponseTypes
+    {
+        public static $ok = "200";
+        public static $unauth = "401";
+    }
